3bc13104a1742b1987660f363c309e413fbdc2f6
SPDiw <swaroopdiwakar007@gmail.com>
2023-01-16 15:18:21 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import threading
 import socket
 
 host = '127.0.0.1'
-port = 55555
+port = 9090
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
@@ -17,37 +17,40 @@ def broadcast(message):
         client.send(message)
 
 
-def handle(client):
-    while True:
-        try:
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast(f'{nickname} left the chat'.encode('ascii'))
-            nicknames.remove(nickname)
-            break
-
-
 def receive():
     while True:
         client, address = server.accept()
         print(f"Connected with {str(address)}")
 
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+
         nicknames.append(nickname)
         clients.append(client)
 
         print(f'Nickname of client is {nickname}')
-        broadcast(f'{nickname} joined the chat'.encode('ascii'))
-        client.send('Connected to the server!'.encode('ascii'))
+        broadcast(f'{nickname} joined the chat'.encode('utf-8'))
+        client.send('Connected to the server!'.encode('utf-8'))
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+
+        except:
+            if client in clients:
+                index = clients.index(client)
+                clients.remove(client)
+                client.close()
+                nickname = nicknames[index]
+                broadcast(f'{nickname} left the chat'.encode('utf-8'))
+                nicknames.remove(nickname)
+                break
 
 
 print('server is listening')
